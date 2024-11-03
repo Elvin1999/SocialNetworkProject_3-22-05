febaf6f6120069190caf6a63983f13e2775881d8
camalzadeelvin <camalzadeelvin@STHQ0126-01>
2024-11-03 13:01:33 +0400

    some changes

---
+++ SocialNetworkProject_3-22-05/Migrations/20241103082229_somechanges.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SocialNetworkProject_3_22_05.Migrations
+{
+    /// <inheritdoc />
+    public partial class somechanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
